27301baa7369ec2e515b4e3fae8f118f75caa88b
Dheeraj Thind <dheerajthind@gmail.com>
2023-09-12 20:20:59 -0400

    fixed a bug with the server

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const api = require('./routes/index.js');
 
 const app = express();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public/')))
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api)
-
-
-
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
